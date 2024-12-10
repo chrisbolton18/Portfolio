4e89be5c327e94615955e7b4fba2565247d10601
chrisbolton18 <chris8bolton@icloud.com>
2024-12-10 11:06:41 -0500

    Fix TypeScript errors and update project structure

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
     unoptimized: true,
   },
+  assetPrefix: '/Portfolio/',
+  basePath: '/Portfolio',
 }
 
 module.exports = nextConfig
